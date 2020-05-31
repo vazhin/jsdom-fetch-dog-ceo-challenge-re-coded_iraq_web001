778482ce9e997ce825bec2310af9fac4f810d22e
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-31 14:30:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+fetch(imgUrl)
+.then(response => response.json())
+.then(json){
+  console.log(json);
+}
