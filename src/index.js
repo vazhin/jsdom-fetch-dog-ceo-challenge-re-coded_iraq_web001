5c482683906f1c3199b2898ad7b08414caefb7d9
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-31 15:10:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('domContentLoaded' () => {
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  console.log('%c HI', 'color: firebrick')
 
-fetch(imgUrl)
-.then(response => response.json())
-.then(result => {
-  let imgContainer = document.querySelector('#dog-image-container');
-  for (const element of result.message){
-    let img = document.createElement('img');
-    img.src = `${element}`;
-    img.width = "200";
-    imgContainer.appendChild(img);
-  }
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+  fetch(imgUrl)
+  .then(response => response.json())
+  .then(result => {
+    let imgContainer = document.querySelector('#dog-image-container');
+    for (const element of result.message){
+      let img = document.createElement('img');
+      img.src = `${element}`;
+      img.width = "200";
+      imgContainer.appendChild(img);
+    }
+  })
+
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+  .then(response => response.json())
+  .then(result => {
+    let breedContainer = document.querySelector('#dog-breeds');
+    for (const key in result.message){
+      let breed = document.createElement('li');
+      breed.innerText = key;
+      breedContainer.appendChild(breed);
+      breed.addEventListener('click', () => breed.style.color = 'red')
+    }
+  })
+
+  let breedDropdown = document.querySelector('#breed-dropdown');
+
+  let option = document.querySelectorAll('option')
+
+  console.log(option.value);
+
+  
 })
 
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-fetch(breedUrl)
-.then(response => response.json())
-.then(result => {
-  let breedContainer = document.querySelector('#dog-breeds');
-  for (const key in result.message){
-    let breed = document.createElement('li');
-    breed.innerText = key;
-    breedContainer.appendChild(breed);
-    breed.addEventListener('click', () => breed.style.color = 'red')
-  }
-})
 
-let breedDropdown = document.querySelector('#breed-dropdown');
-
-let option = document.querySelectorAll('option')
-
-console.log(option.value);
