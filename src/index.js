ca8da5cff7bb4019b880ffadd22d9c7028ff25f6
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-31 14:51:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 fetch(imgUrl)
 .then(response => response.json())
 .then(result => {
+  let imgContainer = document.querySelector('#dog-image-container');
   console.log(result.message);
   for (const element of result.message){
     console.log(element);
-    let imgContainer = document.querySelector('#dog-image-container');
     let img = document.createElement('img');
     img.src = `${element}`;
     imgContainer.appendChild(img);
