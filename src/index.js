a129b59f2da1a0d76c2710d1f9ddff85c041e3b4
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-31 15:45:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   .then(response => response.json())
   .then(result => updateBreeds)
 
-  })
+  )
 
 
   let breedDropdown = document.querySelector('#breed-dropdown');
