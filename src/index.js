76d22ce170fda69b07abae7425d43a56e4581054
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-31 14:51:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ fetch(imgUrl)
 .then(response => response.json())
 .then(result => {
   let imgContainer = document.querySelector('#dog-image-container');
-  console.log(result.message);
   for (const element of result.message){
-    console.log(element);
     let img = document.createElement('img');
     img.src = `${element}`;
     imgContainer.appendChild(img);
